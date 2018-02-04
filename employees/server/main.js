4818f983efec41ef9eff8fbf65046010bee59dc4
PicassoTrigger <david@picassotrigger.com>
2018-02-04 11:22:18 -0600

    completed employee project

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,5 +1,34 @@
+// Only executed on the server
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
+
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	// Great place to generate data
+
+	// Check to see if data exists in the collection
+	// See if collection has any records
+	const numberRecords = Employees.find({}).count();
+	console.log(numberRecords);
+
+	if(!numberRecords) {
+		// Generate some data
+		_.times(5000, () => {
+			const { name, email, phone } = helpers.createCard();
+
+			Employees.insert({
+				name: name,
+				email: email,
+				phone: phone,
+				avatar: image.avatar()
+			});
+		});
+	};
+
+
+	Meteor.publish('employees', function(per_page) {
+		return Employees.find({}, { limit: per_page });
+	});
 });
